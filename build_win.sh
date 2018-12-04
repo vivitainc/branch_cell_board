26b0b43ad3da2b12c020ba5f263135c88c430553
Tomohiro Hayashi <hayashi@vivita.co>
2018-12-04 15:53:18 +0900

    build script change to use make instead of mingw32-make refs #468

--- build_win.sh
+++ build_win.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 PROGNAME=$(basename $0)
-VERSION="${PROGNAME} v1.0"
+VERSION="${PROGNAME} v1.1"
 echo
 echo ${VERSION}
 echo
 
 cd bootloaders/optiboot
-mingw32-make.exe clean
-mingw32-make.exe atmega328pb_8mhz
+make clean
+make atmega328pb_8mhz
 cp optiboot_atmega328pb_8mhz.hex ${USERPROFILE}/AppData/Local/Arduino15/packages/pololu-a-star/hardware/avr/4.0.2/bootloaders/optiboot
 cd ../../
 
